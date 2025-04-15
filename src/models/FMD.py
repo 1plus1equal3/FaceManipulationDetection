b142fe3b960c377d49f6137ee5e84570663b9a84
tungduong123q@gmail.com <tungduong123q@gmail.com>
2025-04-15 23:14:23 +0700

    Fix

--- src/models/FMD.py
+++ src/models/FMD.py
@@ -11,13 +11,14 @@ import torchvision
 from tqdm import tqdm
 from src.network.backbone_gan import *
 from src.network.u2net_gan import *
+from src.utils.perceptural_loss import *
 
 class FMD(nn.Module):
     def __init__(self, device, in_channels=3):
         super(FMD, self).__init__()
-        
         self.device = device
         
+        # init model
         self.u2net_gan = init_u2net_gan()
         self.disc = init_discriminator()
         
@@ -38,7 +39,8 @@ class FMD(nn.Module):
         
         # loss function
         self.criterion_segment = nn.BCELoss(size_average=True)
-        self.criterion_gan = nn.MSELoss()
+        self.criterion_disc = nn.MSELoss()
+        self.criterion_gan = VGG19PerceptureLoss()
         self.criterion_rec = nn.L1Loss()
         
     def forward(self):
@@ -47,9 +49,10 @@ class FMD(nn.Module):
 
         return self.rec_img, self.d0, self.d1, self.d2, self.d3, self.d4, self.d6
         
-    def set_input(self, inputs, labels, input_is_real):
+    def set_input(self, inputs, labels, real_images=None, input_is_real=None):
         self.inputs = inputs.to(self.device)
         self.labels = labels.to(self.device)
+        self.real_images = real_images
         self.input_is_real = input_is_real.to(self.device)
     
     def get_label_from_input(self, input, input_is_real):
@@ -71,7 +74,7 @@ class FMD(nn.Module):
         """ calculate loss for u2net-gan
         """
         
-        # loss for segmentation
+        # segmentation loss
         loss_d0 = self.criterion_segment(self.d0, self.labels)
         loss_d1 = self.criterion_segment(self.d1, self.labels)
         loss_d2 = self.criterion_segment(self.d2, self.labels)
@@ -82,12 +85,19 @@ class FMD(nn.Module):
         loss_seg = loss_d0 + loss_d1 + loss_d2 + loss_d3 + loss_d4 + loss_d6
         
         # gan loss (D(G(x)) -> 1)
-        loss_gen = self.criterion_rec(self.disc(self.rec_img), torch.ones_like(self.disc(self.rec_img)))
+        loss_gen = self.criterion_gan(self.disc(self.rec_img), torch.ones_like(self.disc(self.rec_img)))
         
-        loss_u2net_gan =loss_seg + loss_gen
+        # reconstruct loss
+        if self.real_images is not None:
+            loss_rec = self.criterion_rec(self.rec_img, self.real_images)
+        else:
+            loss_rec = 0
+        
+        # conbine loss u2net-gan
+        loss_u2net_gan =loss_seg + loss_gen + loss_rec
         loss_u2net_gan.backward()
         
-        return loss_seg, loss_gen
+        return loss_seg, loss_gen, loss_rec
     
     def backward_disc(self):
         """ calculate loss for discriminator
@@ -96,13 +106,13 @@ class FMD(nn.Module):
         # dis criminator loss
         pred_rec = self.disc(self.rec_img.detach())      # pass rec into discriminator
         
-        rec_label = self.get_label_from_input(pred_rec, self.input_is_real)
-        input_label = self.get_label_from_input(self.inputs, True)
+        # rec_label = self.get_label_from_input(pred_rec, self.input_is_real)
+        # input_label = self.get_label_from_input(self.inputs, True)
         
-        loss_dis = (self.criterion_gan(pred_rec, rec_label) + self.criterion_gan(self.inputs, input_label)) * 0.5
+        loss_dis = (self.criterion_disc(pred_rec, torch.ones_like(pred_rec)) + self.criterion_disc(self.inputs, torch.zeros_like(self.inputs))) * 0.5
         
         loss_dis.backward()
-        return self.criterion_gan(pred_rec, rec_label), self.criterion_gan(self.inputs, input_label)
+        return self.criterion_disc(pred_rec, torch.ones_like(pred_rec)), self.criterion_disc(self.inputs, torch.zeros_like(self.inputs))
     
     def optimize_parameters(self):
         # pass data into network
@@ -110,7 +120,7 @@ class FMD(nn.Module):
         # train u2net-gan
         self.optimizer_u2net_gan.zero_grad()
         self.set_requires_grad([self.disc], False)      # freeze disc when training u2net-gan
-        loss_seg, loss_gen = self.backward_u2net_gan()
+        loss_seg, loss_gen, loss_rec = self.backward_u2net_gan()
         self.optimizer_u2net_gan.step()
         
         # train disc
@@ -119,4 +129,15 @@ class FMD(nn.Module):
         loss_D_fake, loss_D_real = self.backward_disc()
         self.optimizer_disc.step()
         
-        return loss_seg, loss_gen, loss_D_fake, loss_D_real
+        return loss_seg, loss_gen, loss_rec, loss_D_fake, loss_D_real
+    
+    def optimize_segmentation(self):
+        # pass data into network
+        self.forward()
+        # train u2net
+        self.optimizer_u2net_gan.zero_grad()
+        self.set_requires_grad([self.disc], False)
+        loss_seg, _, _ = self.backward_u2net_gan()
+        self.optimizer_u2net_gan.step()
+        
+        return loss_seg
