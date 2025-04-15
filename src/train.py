6a2df28bd72cf890a6407a3f1cd5fbbb0e41edab
tungduong123q@gmail.com <tungduong123q@gmail.com>
2025-04-15 23:15:30 +0700

    Update: Adding 2 phase for training loop (real only and fake only)

--- src/train.py
+++ src/train.py
@@ -23,22 +23,26 @@ def load_config(config_path):
 config = load_config('src/config.json')
 
 def main():
-    train_loader = get_dataloader(mode='train')
-    test_loader = get_dataloader(mode='test')
+    train_real_loader = get_dataloader(mode='train', is_real=True)
+    train_fake_loader = get_dataloader(mode='train', is_real=False)
+    test_real_loader = get_dataloader(mode='test', is_real=True)
+    test_fake_loader = get_dataloader(mode='test', is_real=False)
     
     # define model
     fmd = FMD(device=device)
     
+    # phase 1: train with real image
     epochs = config['model']['epoch']
     for epoch in range(epochs):
         total_loss_seg = 0.0
         total_loss_gen = 0.0
         total_loss_D_fake = 0.0
         total_loss_D_real = 0.0
+        total_loss_rec = 0.0
         
-        for (input, true_mask, input_is_real) in tqdm(train_loader):
-            fmd.set_input(input, true_mask, input_is_real)
-            loss_seg, loss_gen, loss_D_fake, loss_D_real = fmd.optimize_parameters()
+        for (input, true_mask) in tqdm(train_real_loader):
+            fmd.set_input(input, true_mask)
+            loss_seg, loss_gen, _, loss_D_fake, loss_D_real = fmd.optimize_parameters()
             
             # total loss
             total_loss_seg += loss_seg
@@ -47,15 +51,62 @@ def main():
             total_loss_D_real += loss_D_real
             
         if (epoch + 1) % 10 == 0 or epoch == 0:
-            print(f"loss_seg: {total_loss_seg/len(train_loader):.4f}, loss_gen: {total_loss_gen/len(train_loader):.4f}, loss_D_fake: {total_loss_D_fake/len(train_loader):.4f}, loss_D_real: {total_loss_D_real/len(train_loader):.4f}")
+            print(f"loss_seg: {total_loss_seg/len(train_real_loader):.4f}, loss_gen: {total_loss_gen/len(train_real_loader):.4f}, loss_rec: {total_loss_rec/len(train_real_loader):.4f}, loss_D_fake: {total_loss_D_fake/len(train_real_loader):.4f}, loss_D_real: {total_loss_D_real/len(train_real_loader):.4f}")
+            
+            # save checkpoint
+            if not os.path.exists('checkpoints'):
+                os.makedirs('checkpoints')
+                
+            torch.save(fmd.state_dict(), 'checkpoints/weights_v2.pt')
             
             # visualize some sample in test loader
             with torch.no_grad():
-                for input, true_mask, input_is_real in test_loader:
-                    fmd.set_input(input, true_mask, input_is_real)
+                for input, true_mask in test_real_loader:
+                    fmd.set_input(input, true_mask)
                     rec_img, pred_mask, _, _, _, _, _ = fmd()
                     
                     visualize_results(input, rec_img, true_mask, pred_mask, epoch)
                     break
+                
+                
+    # phase 2: train with fake image
+    epochs = config['model']['epoch']
+    for epoch in range(epochs):
+        total_loss_seg = 0.0
+        total_loss_gen = 0.0
+        total_loss_D_fake = 0.0
+        total_loss_D_real = 0.0
+        total_loss_rec = 0.0
+        
+        for (input, true_mask, real_image) in tqdm(train_fake_loader):
+            fmd.set_input(input, true_mask, real_image)
+            loss_seg, loss_gen, loss_rec, loss_D_fake, loss_D_real = fmd.optimize_parameters()
+            
+            # total loss
+            total_loss_seg += loss_seg
+            total_loss_gen += loss_gen
+            total_loss_rec += loss_rec
+            total_loss_D_fake += loss_D_fake
+            total_loss_D_real += loss_D_real
+            
+        if (epoch + 1) % 10 == 0 or epoch == 0:
+            print(f"loss_seg: {total_loss_seg/len(train_real_loader):.4f}, loss_gen: {total_loss_gen/len(train_real_loader):.4f}, loss_rec: {total_loss_rec/len(train_real_loader):.4f}, loss_D_fake: {total_loss_D_fake/len(train_real_loader):.4f}, loss_D_real: {total_loss_D_real/len(train_real_loader):.4f}")
+            
+            # save checkpoint
+            if not os.path.exists('checkpoints'):
+                os.makedirs('checkpoints')
+                
+            torch.save(fmd.state_dict(), 'checkpoints/weights_v2.pt')
+            
+            # visualize some sample in test loader
+            with torch.no_grad():
+                for input, true_mask in test_fake_loader:
+                    fmd.set_input(input, true_mask)
+                    rec_img, pred_mask, _, _, _, _, _ = fmd()
+                    
+                    visualize_results(input, rec_img, true_mask, pred_mask, epoch)
+                    break
+                
+
 if __name__ == '__main__':
     main()
