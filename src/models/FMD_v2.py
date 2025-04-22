cfe00d8e778b8805f93adbcf69ccdebaf8dab186
tungduong123q@gmail.com <tungduong123q@gmail.com>
2025-04-22 17:06:50 +0700

    Feat: FMD v2

---
+++ src/models/FMD_v2.py
@@ -0,0 +1,88 @@
+import os
+import sys
+import cv2
+sys.path.append(os.getcwd())
+
+import torch
+import torch.nn as nn
+from torch import optim
+import torchvision
+
+from tqdm import tqdm
+from src.network.backbone_gan import *
+from src.network.u2net_gan_v2 import U2NetGanV2, init_u2net_gan_v2
+from src.utils.perceptural_loss import VGG19PerceptureLoss
+
+class FMD_v2(nn.Module):
+    def __init__(self, device, in_channels=3):
+        super(FMD_v2, self).__init__()
+        self.device = device
+        
+        # init model
+        self.u2net_gan = init_u2net_gan_v2()
+        
+        # set training mode
+        self.u2net_gan.train()
+        
+        # use_gpu
+        self.u2net_gan.to(self.device)
+        
+        # define for training
+        # optim
+        self.optimizer_u2net_gan = optim.Adam(self.u2net_gan.parameters(), lr=0.0003, betas=(0.5, 0.999))
+        
+        # scheduler
+        
+        # loss function
+        self.criterion_segment = nn.BCELoss(size_average=True)
+        # self.criterion_rec = VGG19PerceptureLoss()
+        
+    def forward(self):
+        # get output of u2net-gan
+        self.d0, self.d1, self.d2, self.d3, self.d4, self.d6 = self.u2net_gan(self.inputs)
+
+        return self.d0, self.d1, self.d2, self.d3, self.d4, self.d6
+        
+    def set_input(self, inputs, labels, real_images=None, input_is_real=None):
+        self.inputs = inputs.to(self.device)
+        self.labels = labels.to(self.device)
+        self.real_images = real_images.to(self.device) if real_images is not None else real_images
+        self.input_is_real = input_is_real
+        
+    def set_requires_grad(self, nets, requires_grad=True):
+        if not isinstance(nets, list):
+            nets = [nets]
+        for net in nets:
+            if net is not None:
+                for param in net.parameters():
+                    param.requires_grad = requires_grad
+            
+    
+    def backward_u2net_gan_v2(self, lambda_seg=0.1):
+        """ calculate loss for u2net-gan
+        """
+        
+        # segmentation loss
+        loss_d0 = self.criterion_segment(self.d0, self.labels)
+        loss_d1 = self.criterion_segment(self.d1, self.labels)
+        loss_d2 = self.criterion_segment(self.d2, self.labels)
+        loss_d3 = self.criterion_segment(self.d3, self.labels)
+        loss_d4 = self.criterion_segment(self.d4, self.labels)
+        loss_d6 = self.criterion_segment(self.d6, self.labels)
+        
+        loss_seg = loss_d0 + loss_d1 + loss_d2 + loss_d3 + loss_d4 + loss_d6
+
+        loss_seg.backward()
+        
+        return loss_seg
+    
+    
+    def optimize_parameters(self):
+        # pass data into network
+        self.forward()
+        # train u2net-gan
+        self.optimizer_u2net_gan.zero_grad()
+        loss_seg = self.backward_u2net_gan()
+        self.optimizer_u2net_gan.step()
+        
+        return loss_seg
