aef435f7a24ddb8fe4e479d3092e778ca392e862
TranTungDuong1611 <tungduong123q@gmail.com>
2025-05-15 16:40:53 +0700

    Update: train.py

--- src/train.py
+++ src/train.py
@@ -3,14 +3,11 @@ import os
 import sys
 import argparse
 import random
+import numpy as np
 
 sys.path.append(os.getcwd())
 
 import torch
-import torch.distributed as dist
-import torch.nn as nn
-import torch.optim as optim
-from torch.nn.parallel import DistributedDataParallel as DDP
 from tqdm import tqdm
 
 from src.data.dataloader import get_dataloader
@@ -18,6 +15,14 @@ from src.models.FMD_v2 import *
 from src.utils.util import *
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(device)
+
+def load_config(config_path):
+    with open(config_path, 'r') as f:
+        config = json.load(f)
+    return config
+
+config = load_config('src/config.json')
 
 def set_random_seed(seed):
     random.seed(seed)
@@ -30,20 +35,15 @@ def set_random_seed(seed):
     import os
     os.environ['PYTHONHASHSEED'] = str(seed)
 
-def load_config(config_path):
-    with open(config_path, 'r') as f:
-        config = json.load(f)
-    return config
-
-config = load_config('src/config.json')
-
 def main():
-    # random seed
+    # set random seed
     set_random_seed(42)
 
-    # parser argument
+    # argument parser
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset_path', type=str, default='', help='path to your dataset')
+    parser.add_argument('--weights', type=str, default=None, help='path to your pretrained weight')
+    parser.add_argument('--resume_epoch', type=int, default=0, help='resume epoch if using pretrained')
     parser.add_argument('--save_results', type=str, default='', help='path to save results')
     
     args = parser.parse_args()
@@ -51,11 +51,20 @@ def main():
     # define model
     fmd_v2 = FMD_v2(device=device)
     
+    # load pre-trained weight
+    if args.weights:
+        model_dict = torch.load(args.weights, weights_only=True)
+        
+        # load weight
+        pretrain_dict = {k: v for k, v in model_dict.items() if k in fmd_v2.state_dict()}
+        fmd_v2.load_state_dict(pretrain_dict, strict=False)
+    
     # dataloader
-    train_real_loader = get_dataloader(args.dataset_path, mode='train', is_real=True)
-    train_fake_loader = get_dataloader(args.dataset_path, mode='train', is_real=False)
-    test_real_loader = get_dataloader(args.dataset_path, mode='test', is_real=True)
-    test_fake_loader= get_dataloader(args.dataset_path, mode='test', is_real=False)
+    train_combined_loader = get_dataloader(dataset_path=args.dataset_path, mode='train', type='combined')
+    test_combined_loader = get_dataloader(dataset_path=args.dataset_path, mode='test', type='combined')
+    bald_loader = get_dataloader(dataset_path=args.dataset_path, attribute='bald')
+    eyeglass_loader = get_dataloader(dataset_path=args.dataset_path, attribute='eyeglass')
+    smile_loader = get_dataloader(dataset_path=args.dataset_path, attribute='smile')
     
     if not os.path.exists('checkpoints'):
         os.makedirs('checkpoints')
@@ -100,56 +109,109 @@ def main():
     #         print(f"loss_seg_train: {total_loss_seg/len(train_real_loader):.4f}\t loss_seg_val: {total_loss_seg_val/len(test_real_loader):.4f}") 
                 
     # phase 2: train with fake image
-    epochs = config['model']['epoch']
     best_loss_seg = 1e6
     count = 0
-    for epoch in tqdm(range(50)):
+    for epoch in tqdm(range(config['model']['epoch'])):
+        # set training mode
         fmd_v2.train()
+        
         torch.cuda.empty_cache()
         total_loss_seg = 0.0
+        total_loss_cls = 0.0
+        true_preds = 0
+        total = 0
 
-        for (input, true_mask, real_image) in train_fake_loader:
-            fmd_v2.set_input(input, true_mask, real_image)
+        for (input, true_mask, ela, true_label) in train_combined_loader:
+            fmd_v2.set_input(inputs=input, segment_labels=true_mask, ela=ela, cls_labels=true_label)
+            
             loss_seg = fmd_v2.optimize_parameters()
             
             # total loss
             total_loss_seg += loss_seg.item()
+            break
             
-        # eval    
-        # visualize some sample in test loader
+        # eval
         fmd_v2.eval()
+        # visualize some sample in test loader
         total_loss_seg_val = 0.0
         total_psnr = 0.0
+        total_ssim = 0.0
+        psnr_bald = 0
+        psnr_eyeglass = 0
+        psnr_smile = 0
         with torch.no_grad():
-            for i, (input, true_mask, real_image) in enumerate(test_fake_loader):
-                fmd_v2.set_input(input, true_mask, real_image)
-                pred_mask, _, _, _, _, _, _ = fmd_v2()
+            for i, (input, true_mask, ela, true_label) in enumerate(test_combined_loader):
+                fmd_v2.set_input(inputs=input, segment_labels=true_mask, ela=ela, cls_labels=true_label)
+                pred_mask, d1, d2, d3, d4, d5, d6 = fmd_v2()
                 
                 if ((epoch + 1) % 5 == 0 or epoch == 0) and i == 20:
-                    visualize_results(input, real_image, true_mask, pred_mask, args.save_results, epoch+1, text='phase_2')
-                
-                # segmemtation loss
-                loss = fmd_v2.calculate_loss(pred_mask, true_mask)
-                total_loss_seg_val += loss.item()
+                    visualize_results(input, input, true_mask, pred_mask, d1, d2, d3, d4, d5, d6, args.save_results, epoch+1)
+ 
+                # loss
+                loss_seg, loss_cls = fmd_v2.backward_u2net_gan_v2(training=False)
+                total_loss_seg_val += loss_seg.item()
+
                 
                 # psnr
                 psnr = compute_psnr_batch(true_mask, pred_mask, device=device)
                 total_psnr += psnr.item()
+                
+                # ssim
+                ssim = ssim_batch(pred_mask, true_mask)
+                total_ssim += ssim.item()
+                break
+
+            if (epoch + args.resume_epoch + 1) % 5 == 0 or epoch == 0:
+                # psnr for bald
+                for (input, true_mask, ela, true_label) in bald_loader:
+                    fmd_v2.set_input(inputs=input, segment_labels=true_mask, ela=ela, cls_labels=true_label)
+                    pred_mask, d1, d2, d3, d4, d5, d6 = fmd_v2()
+
+                    # psnr
+                    psnr = compute_psnr_batch(true_mask, pred_mask, device=device)
+                    psnr_bald += psnr.item()
+
+                # psnr for eyeglass
+                for (input, true_mask, ela, true_label) in eyeglass_loader:
+                    fmd_v2.set_input(inputs=input, segment_labels=true_mask, ela=ela, cls_labels=true_label)
+                    pred_mask, d1, d2, d3, d4, d5, d6 = fmd_v2()
+
+                    # psnr
+                    psnr = compute_psnr_batch(true_mask, pred_mask, device=device)
+                    psnr_eyeglass += psnr.item()
+
+                # psnr for smile
+                for (input, true_mask, ela, true_label) in smile_loader:
+                    fmd_v2.set_input(inputs=input, segment_labels=true_mask, ela=ela, cls_labels=true_label)
+                    pred_mask, d1, d2, d3, d4, d5, d6 = fmd_v2()
+
+                    # psnr
+                    psnr = compute_psnr_batch(true_mask, pred_mask, device=device)
+                    psnr_smile += psnr.item()
         
-        print(f"loss_seg_train: {total_loss_seg/len(train_fake_loader):.4f}\t loss_seg_val: {total_loss_seg_val/len(test_fake_loader):.4f}\t \
-            psnr: {total_psnr/len(test_fake_loader):.4f}")
+        print(
+            f"\nEpoch: {epoch + args.resume_epoch + 1}\n"
+            f"loss_seg_train: {total_loss_seg/len(train_combined_loader):.4f}\n"
+            f"loss_seg_val: {total_loss_seg_val/len(test_combined_loader):.4f}\n"
+            f"loss_cls_train: {total_loss_cls/len(train_combined_loader):.4f}\n"
+            f"acc_train: {true_preds/total:.4f}\n"
+            f"total_psnr: {total_psnr/len(test_combined_loader):.4f}\n"
+            f"ssim: {total_ssim/len(test_combined_loader):.4f}\n"
+            f"bald_psnr: {psnr_bald/len(bald_loader):.4f}\n"
+            f"eyeglass_psnr: {psnr_eyeglass/len(eyeglass_loader):.4f}\n"
+            f"smile_psnr: {psnr_smile/len(smile_loader):.4f}\n"
+        )
         
         # Early Stopping
         if total_loss_seg_val < best_loss_seg:
             best_loss_seg = total_loss_seg_val
             count = 0
-            # save checkpoint
             checkpoint_path = f'checkpoints/weights_attn_gate.pt'
             torch.save(fmd_v2.state_dict(), checkpoint_path)
-                
-        elif abs(total_loss_seg_val - best_loss_seg) < 0.001 or (total_loss_seg_val - best_loss_seg) > 0.01:
+        else:
             count += 1
-            if count == 5:
+            if count >= 5:
+                print(f"Early Stopping after {epoch + args.resume_epoch + 1} epochs\n")
                 break
         
 if __name__ == '__main__':
