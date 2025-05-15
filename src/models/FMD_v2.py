65a4c34777b7c6cca8a2d1e671380bc8a90afc66
TranTungDuong1611 <tungduong123q@gmail.com>
2025-05-15 09:48:36 +0700

    Update: weight for seg and cls

--- src/models/FMD_v2.py
+++ src/models/FMD_v2.py
@@ -6,12 +6,10 @@ sys.path.append(os.getcwd())
 import torch
 import torch.nn as nn
 from torch import optim
-import torchvision
 
 from tqdm import tqdm
 from src.network.backbone_gan import *
 from src.network.u2net_gan_v2 import init_u2net_gan_v2
-from src.utils.perceptural_loss import VGG19PerceptureLoss
 
 def load_config(config_path):
     with open(config_path, 'r') as f:
@@ -28,34 +26,31 @@ class FMD_v2(nn.Module):
         # init model
         self.u2net_gan_v2 = init_u2net_gan_v2()
         
-        # set training mode
-        self.u2net_gan_v2.train()
-        
         # use_gpu
         self.u2net_gan_v2.to(self.device)
         
         # define for training
         # optim
-        self.optimizer_u2net_gan_v2 = optim.Adam(self.u2net_gan_v2.parameters(), lr=config['model']['lr'], betas=(0.9, 0.999))
+        self.optimizer_u2net_gan_v2 = optim.Adam(self.u2net_gan_v2.parameters(), lr=0.005, betas=(0.9, 0.999))
         
         # scheduler
         
         # loss function
         self.criterion_segment = nn.L1Loss()
-        self.criterion_cls = nn.BCEWithLogitsLoss()
+        self.criterion_cls = nn.CrossEntropyLoss()
         # self.criterion_rec = VGG19PerceptureLoss()
         
     def forward(self):
         # get output of u2net-gan
-        self.d0, self.d1, self.d2, self.d3, self.d4, self.d5, self.d6 = self.u2net_gan_v2(self.inputs)
+        self.d0, self.d1, self.d2, self.d3, self.d4, self.d5, self.d6, self.pred_label = self.u2net_gan_v2(self.inputs, self.ela)
 
-        return self.d0, self.d1, self.d2, self.d3, self.d4, self.d5, self.d6
+        return self.d0, self.d1, self.d2, self.d3, self.d4, self.d5, self.d6, self.pred_label
         
     def set_input(self, inputs, segment_labels, ela=None, cls_labels=None, real_images=None):
         self.inputs = inputs.to(self.device)
         self.segment_labels = segment_labels.to(self.device)
-        self.ela = ela.to(self.device) if ela is not None else ela
-        self.cls_labels = cls_labels.to(self.device) if cls_labels is not None else cls_labels
+        self.ela = ela.to(self.device)
+        self.cls_labels = cls_labels.to(self.device)
         self.real_images = real_images.to(self.device) if real_images is not None else real_images
         
     def set_requires_grad(self, nets, requires_grad=True):
@@ -67,40 +62,63 @@ class FMD_v2(nn.Module):
                     param.requires_grad = requires_grad
             
     
-    def backward_u2net_gan_v2(self, lambda_seg=0.1):
+    def backward_u2net_gan_v2(self, training=True, lambda_seg=20, lambda_cls=1):
         """ calculate loss for u2net-gan
         """
-
-        # segmentation loss
-        loss_d0 = self.criterion_segment(self.d0, self.segment_labels)
-        loss_d1 = self.criterion_segment(self.d1, self.segment_labels)
-        loss_d2 = self.criterion_segment(self.d2, self.segment_labels)
-        loss_d3 = self.criterion_segment(self.d3, self.segment_labels)
-        loss_d4 = self.criterion_segment(self.d4, self.segment_labels)
-        loss_d5 = self.criterion_segment(self.d5, self.segment_labels)
-        loss_d6 = self.criterion_segment(self.d6, self.segment_labels)
-
-        loss_seg = loss_d0 + loss_d1 + loss_d2 + loss_d3 + loss_d4 + loss_d5 + loss_d6
-
-        loss_seg.backward()
         
-        return loss_seg
+        # segmentation loss
+        loss_seg_d0 = self.criterion_segment(self.d0, self.segment_labels)
+        loss_seg_d1 = self.criterion_segment(self.d1, self.segment_labels)
+        loss_seg_d2 = self.criterion_segment(self.d2, self.segment_labels)
+        loss_seg_d3 = self.criterion_segment(self.d3, self.segment_labels)
+        loss_seg_d4 = self.criterion_segment(self.d4, self.segment_labels)
+        loss_seg_d5 = self.criterion_segment(self.d5, self.segment_labels)
+        loss_seg_d6 = self.criterion_segment(self.d6, self.segment_labels)
+
+        self.loss_seg = loss_seg_d0 + 0.6 * loss_seg_d1 + 0.2 * (loss_seg_d2 + loss_seg_d3 + loss_seg_d4 + loss_seg_d5 + loss_seg_d6)
+        
+        # classification loss
+        self.loss_cls = self.criterion_cls(self.pred_label, self.cls_labels)
+
+        # combine loss
+        total_loss = self.loss_seg * lambda_seg + lambda_cls * self.loss_cls
+        if training:
+            total_loss.backward()
+        
+        return self.loss_seg * lambda_seg, lambda_cls * self.loss_cls
     
     
-    def optimize_parameters(self):
+    def optimize_parameters(self, freeze_cls_branch=False): 
+        """ optimizer for u2net-gan
+        """
+        # freeze cls branch
+        if freeze_cls_branch:
+            for name, param in self.u2net_gan_v2.named_parameters():
+                if 'classifier' in name or 'encode_ela' in name:
+                    param.requires_grad = False
+                else:
+                    param.requires_grad = True
+        else:
+            for name, param in self.u2net_gan_v2.named_parameters():
+                param.requires_grad = True
+    
         # pass data into network
         self.forward()
         # train u2net-gan
         self.optimizer_u2net_gan_v2.zero_grad()
-        loss_seg = self.backward_u2net_gan_v2()
+        self.loss_seg, self.loss_cls = self.backward_u2net_gan_v2()
         self.optimizer_u2net_gan_v2.step()
-
-        return loss_seg
-
-    def calculate_loss(self, pred, gt):
-        pred = pred.to(self.device)
-        gt = gt.to(self.device)
-
-        loss = self.criterion_segment(pred, gt)
         
-        return loss
+        return self.loss_seg, self.loss_cls
+    
+    def get_loss(self):
+        return self.loss_seg, self.loss_cls
+    
+    def get_num_true_pred_images(self):
+        predicted_classes = torch.argmax(self.pred_label, dim=1)  # shape: [N]
+
+        true_classes = self.cls_labels.view(-1)
+    
+        correct = (predicted_classes == true_classes).sum().item()
+        total = true_classes.size(0)
+        return correct, total
