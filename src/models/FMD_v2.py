43042b6f11e6a676894ca6f7a0d646351255ec98
TranTungDuong1611 <tungduong123q@gmail.com>
2025-05-16 00:13:46 +0700

    Update FMD_v2

--- src/models/FMD_v2.py
+++ src/models/FMD_v2.py
@@ -66,7 +66,7 @@ class FMD_v2(nn.Module):
         loss_d5 = self.criterion_segment(self.d5, self.labels)
         loss_d6 = self.criterion_segment(self.d6, self.labels)
         
-        loss_seg = loss_d0 + 0.8 * loss_d1 + 0.2 * (loss_d2 + loss_d3 + loss_d4 + loss_d5 + loss_d6)
+        loss_seg = 2 * loss_d0 + loss_d1 + 0.4 * (loss_d2 + loss_d3 + loss_d4 + loss_d5 + loss_d6)
 
         if training:
             loss_seg.backward()
