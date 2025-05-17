7a7ab66da6fe9a01f2755dd16712e572ca26294d
TranTungDuong1611 <tungduong123q@gmail.com>
2025-05-17 10:26:51 +0700

    Feat: replace L1Loss with BCE + Dice Loss

--- src/models/FMD_v2.py
+++ src/models/FMD_v2.py
@@ -11,12 +11,40 @@ from tqdm import tqdm
 from src.network.backbone_gan import *
 from src.network.u2net_gan_v2 import init_u2net_gan_v2
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 def load_config(config_path):
     with open(config_path, 'r') as f:
         config = json.load(f)
     return config
 
 config = load_config('src/config.json')
+
+class BCEDiceLoss(nn.Module):
+    def __init__(self, bce_weight=0.5, dice_weight=0.5, smooth=1.0):
+        super(BCEDiceLoss, self).__init__()
+        self.bce_weight = bce_weight
+        self.dice_weight = dice_weight
+        
+        pos_weight = torch.tensor([5.0]).to(device)
+        self.bce = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
+        self.smooth = smooth
+ 
+    def forward(self, logits, targets):
+        # BCE Loss
+        bce = self.bce(logits, targets)
+
+        # Dice Loss
+        probs = torch.sigmoid(logits)
+        targets = targets.type_as(probs)
+
+        intersection = (probs * targets).sum(dim=(1, 2, 3))
+        union = probs.sum(dim=(1, 2, 3)) + targets.sum(dim=(1, 2, 3))
+
+        dice = 1 - (2. * intersection + self.smooth) / (union + self.smooth)
+        dice = dice.mean()
+
+        return self.bce_weight * bce + self.dice_weight * dice
 
 class FMD_v2(nn.Module):
     def __init__(self, device):
@@ -31,12 +59,12 @@ class FMD_v2(nn.Module):
         
         # define for training
         # optim
-        self.optimizer_u2net_gan_v2 = optim.Adam(self.u2net_gan_v2.parameters(), lr=0.005, betas=(0.9, 0.999))
+        self.optimizer_u2net_gan_v2 = optim.Adam(self.u2net_gan_v2.parameters(), lr=1e-4, betas=(0.9, 0.999))
         
         # scheduler
         
         # loss function
-        self.criterion_segment = nn.L1Loss()
+        self.criterion_segment = BCEDiceLoss()
         self.criterion_cls = nn.CrossEntropyLoss()
         # self.criterion_rec = VGG19PerceptureLoss()
         
@@ -75,7 +103,7 @@ class FMD_v2(nn.Module):
         loss_seg_d5 = self.criterion_segment(self.d5, self.segment_labels)
         loss_seg_d6 = self.criterion_segment(self.d6, self.segment_labels)
 
-        self.loss_seg = loss_seg_d0 + 0.6 * loss_seg_d1 + 0.2 * (loss_seg_d2 + loss_seg_d3 + loss_seg_d4 + loss_seg_d5 + loss_seg_d6)
+        self.loss_seg = loss_seg_d0 +  loss_seg_d1 + 0.4 * (loss_seg_d2 + loss_seg_d3 + loss_seg_d4 + loss_seg_d5 + loss_seg_d6)
         
         # classification loss
         self.loss_cls = self.criterion_cls(self.pred_label, self.cls_labels)
