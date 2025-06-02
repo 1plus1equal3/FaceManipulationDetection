e6df12a4fb246a7562464f36c594ce2d2b107294
TranTungDuong1611 <tungduong123q@gmail.com>
2025-06-03 00:22:49 +0700

    Feat: Reconstruct u2net

--- src/network/backbone_u2_net.py
+++ src/network/backbone_u2_net.py
@@ -7,19 +7,19 @@ class REBNCONV(nn.Module):
     def __init__(self, in_ch=3, out_ch=3, dirate=1):
         super(REBNCONV, self).__init__()
 
-        # Depthwise convolution
-        self.depthwise = nn.Conv2d(
-            in_ch, in_ch, kernel_size=3, padding=1*dirate, dilation=1*dirate, groups=in_ch)
-        # Pointwise convolution
-        self.pointwise = nn.Conv2d(
-            in_ch, out_ch, kernel_size=1)
+        # # Depthwise convolution
+        # self.depthwise = nn.Conv2d(
+        #     in_ch, in_ch, kernel_size=3, padding=1*dirate, dilation=1*dirate, groups=in_ch)
+        # # Pointwise convolution
+        # self.pointwise = nn.Conv2d(
+        #     in_ch, out_ch, kernel_size=1)
 
+        self.conv = nn.Conv2d(in_ch, out_ch, kernel_size=3, stride=1, padding=1)
         self.bn = nn.BatchNorm2d(out_ch)
         self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
-        x = self.depthwise(x)
-        x = self.pointwise(x)
+        x = self.conv(x)
         x = self.bn(x)
         x = self.relu(x)
         return x
@@ -357,7 +357,7 @@ class U2NET(nn.Module):
         self.side5 = nn.Conv2d(512,out_ch,3,padding=1)
         self.side6 = nn.Conv2d(512,out_ch,3,padding=1)
 
-        self.outconv = nn.Conv2d(6*out_ch,out_ch,1)
+        self.outconv = nn.Conv2d(5*out_ch,out_ch,1)
 
     def forward(self,x):
 
@@ -381,120 +381,9 @@ class U2NET(nn.Module):
 
         #stage 5
         hx5 = self.stage5(hx)
-        hx = self.pool56(hx5)
-
-        #stage 6
-        hx6 = self.stage6(hx)
-        hx6up = _upsample_like(hx6,hx5)
-
-        #-------------------- decoder --------------------
-        hx5d = self.stage5d(torch.cat((hx6up,hx5),1))
-        hx5dup = _upsample_like(hx5d,hx4)
-
-        hx4d = self.stage4d(torch.cat((hx5dup,hx4),1))
-        hx4dup = _upsample_like(hx4d,hx3)
-
-        hx3d = self.stage3d(torch.cat((hx4dup,hx3),1))
-        hx3dup = _upsample_like(hx3d,hx2)
-
-        hx2d = self.stage2d(torch.cat((hx3dup,hx2),1))
-        hx2dup = _upsample_like(hx2d,hx1)
-
-        hx1d = self.stage1d(torch.cat((hx2dup,hx1),1))
-
-
-        #side output
-        d1 = self.side1(hx1d)
-
-        d2 = self.side2(hx2d)
-        d2 = _upsample_like(d2,d1)
-
-        d3 = self.side3(hx3d)
-        d3 = _upsample_like(d3,d1)
-
-        d4 = self.side4(hx4d)
-        d4 = _upsample_like(d4,d1)
-
-        d5 = self.side5(hx5d)
-        d5 = _upsample_like(d5,d1)
-
-        d6 = self.side6(hx6)
-        d6 = _upsample_like(d6,d1)
-
-        d0 = self.outconv(torch.cat((d1,d2,d3,d4,d5,d6),1))
-
-        return F.sigmoid(d0), F.sigmoid(d1), F.sigmoid(d2), F.sigmoid(d3), F.sigmoid(d4), F.sigmoid(d5), F.sigmoid(d6)
-
-### U^2-Net small ###
-class U2NETP(nn.Module):
-
-    def __init__(self,in_ch=3,out_ch=1):
-        super(U2NETP,self).__init__()
-
-        self.stage1 = RSU7(in_ch,16,64)
-        self.pool12 = nn.MaxPool2d(2,stride=2,ceil_mode=True)
-
-        self.stage2 = RSU6(64,16,64)
-        self.pool23 = nn.MaxPool2d(2,stride=2,ceil_mode=True)
-
-        self.stage3 = RSU5(64,16,64)
-        self.pool34 = nn.MaxPool2d(2,stride=2,ceil_mode=True)
-
-        self.stage4 = RSU4(64,16,64)
-        self.pool45 = nn.MaxPool2d(2,stride=2,ceil_mode=True)
-
-        self.stage5 = RSU4F(64,16,64)
-        self.pool56 = nn.MaxPool2d(2,stride=2,ceil_mode=True)
-
-        self.stage6 = RSU4F(64,16,64)
-
-        # decoder
-        self.stage5d = RSU4F(128,16,64)
-        self.stage4d = RSU4(128,16,64)
-        self.stage3d = RSU5(128,16,64)
-        self.stage2d = RSU6(128,16,64)
-        self.stage1d = RSU7(128,16,64)
-
-        self.side1 = nn.Conv2d(64,out_ch,3,padding=1)
-        self.side2 = nn.Conv2d(64,out_ch,3,padding=1)
-        self.side3 = nn.Conv2d(64,out_ch,3,padding=1)
-        self.side4 = nn.Conv2d(64,out_ch,3,padding=1)
-        self.side5 = nn.Conv2d(64,out_ch,3,padding=1)
-        self.side6 = nn.Conv2d(64,out_ch,3,padding=1)
-
-        self.outconv = nn.Conv2d(6*out_ch,out_ch,1)
-
-    def forward(self,x):
-
-        hx = x
-
-        #stage 1
-        hx1 = self.stage1(hx)
-        hx = self.pool12(hx1)
-
-        #stage 2
-        hx2 = self.stage2(hx)
-        hx = self.pool23(hx2)
-
-        #stage 3
-        hx3 = self.stage3(hx)
-        hx = self.pool34(hx3)
-
-        #stage 4
-        hx4 = self.stage4(hx)
-        hx = self.pool45(hx4)
-
-        #stage 5
-        hx5 = self.stage5(hx)
-        hx = self.pool56(hx5)
-
-        #stage 6
-        hx6 = self.stage6(hx)
-        hx6up = _upsample_like(hx6,hx5)
+        hx5dup = _upsample_like(hx5, hx4)
 
         #decoder
-        hx5d = self.stage5d(torch.cat((hx6up,hx5),1))
-        hx5dup = _upsample_like(hx5d,hx4)
 
         hx4d = self.stage4d(torch.cat((hx5dup,hx4),1))
         hx4dup = _upsample_like(hx4d,hx3)
@@ -520,15 +409,13 @@ class U2NETP(nn.Module):
         d4 = self.side4(hx4d)
         d4 = _upsample_like(d4,d1)
 
-        d5 = self.side5(hx5d)
+        d5 = self.side5(hx5)
         d5 = _upsample_like(d5,d1)
 
-        d6 = self.side6(hx6)
-        d6 = _upsample_like(d6,d1)
+        d0 = self.outconv(torch.cat((d1,d2,d3,d4,d5),1))
 
-        d0 = self.outconv(torch.cat((d1,d2,d3,d4,d5,d6),1))
+        return F.sigmoid(d0), F.sigmoid(d1), F.sigmoid(d2), F.sigmoid(d3), F.sigmoid(d4), F.sigmoid(d5)
 
-        return F.sigmoid(d0), F.sigmoid(d1), F.sigmoid(d2), F.sigmoid(d3), F.sigmoid(d4), F.sigmoid(d5), F.sigmoid(d6)
-    
+
 # u2_net = U2NET()
 # summary(u2_net, (3, 256, 256))
